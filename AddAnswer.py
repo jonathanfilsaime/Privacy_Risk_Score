0d26c3d5f65b46cdc20e0cd256c1f768877ca14c
jonathanfilsaime <jonathanfilsaime@gmail.com>
2018-03-07 21:01:29 -0600

    testing the routing

--- AddAnswer.py
+++ AddAnswer.py
@@ -3,4 +3,4 @@ import webapp2
 class AddAnswer(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('AddAnswer Page')
+        self.response.write('wait a minute Page')
--- AddQuestion.py
+++ AddQuestion.py
@@ -1,6 +1,14 @@
 import webapp2
+# import io
 
 class AddQuestion(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('AddQuestion Page')
+        # self.response.write('AddQuestion Page')
+        self.response.write('hey mate Page')
+
+    # def readFile(self):
+    #     # with io.open(fileNameAndPath) as file:
+    #     #     for line in file:
+    #     #         self.response.write(line + "\n")
+    #     self.response.write('hey mate Page')
